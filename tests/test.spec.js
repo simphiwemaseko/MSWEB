bb96440c032cffdaecc82db4a758292e467f2dc6
simphiwemaseko <simphiwemaseko@aol.com>
2024-09-25 18:33:41 +0200

    Added Simple API TEST

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,16 +1,35 @@
-const {test,expect} = require('@playwright/test');
+const {test,expect, request} = require('@playwright/test');
 const { exec } = require('child_process');
 const {landingPage} = require('../pageobjects/landingpage.js');
 const path = require('path');
+const payload ={
+    "location": {
+      "lat": -38.383494,
+      "lng": 33.427362
+    },
+    "accuracy": 51,
+    "name": "Frontline house",
+    "phone_number": "(+91) 983 893 3937",
+    "address": "29, side layout, cohen 09",
+    "types": [
+      "shoe park",
+      "shop"
+    ],
+    "website": "http://google.com",
+    "language": "French-IN"
+  };
 
 let ngServeProcess;
 let username = "SimzTest"
 
 test.beforeAll(async () => {
-    
+
     const cwd = path.join(process.cwd(), 'MSWEB'); 
-    // Start ng serve using npx
-    ngServeProcess = exec('npx ng serve', { cwd });
+       // Start ng serve using npx
+      ngServeProcess = exec('npx ng serve', { cwd });
+
+      // Allow some time for the app to start
+      await new Promise(resolve => setTimeout(resolve, 5000));
 });
 
 test('Verify that the button is present on the landing page.', async ({ page }) => {
@@ -42,6 +61,19 @@ test('Validate that the displayed popup message is correct', async ({ page }) =>
     await lp.validateDialigMessageIsCorrect(username);
     
 });
+
+test('Basic API Test', async () => {
+    const url = "https://rahulshettyacademy.com/maps/api/place/add/json?key=qaclick123";
+    const apiContext = await request.newContext();
+    
+    const response = await apiContext.post(url, {
+        data: payload
+    });
+
+    // Assert that the response is OK (status code 200-299)
+    expect(response.ok()).toBeTruthy(); // Checks if the response is successful
+});
+
 
 test.afterAll(async () => {
     // Kill the ng serve process after tests complete
